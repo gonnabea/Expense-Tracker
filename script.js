1975c0df2e3a3d4cbe43123d3b838b426e0dca99
Choi Jiwon <winning1993@naver.com>
2020-05-28 02:49:07 +0900

    수입, 지출표 표기

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const addForm = document.getElementById("addForm"),
 histories = document.getElementById("histories"),
 recordName = document.getElementById("recordName"),
-recordValue = document.getElementById("recordValue");
+recordValue = document.getElementById("recordValue"),
+income = document.getElementById("income"),
+minus = document.getElementById("minus");
+
+function totalIncome(){
+    const recordValues = document.getElementsByClassName("record_value");
+    let incomeValue = 0;
+    let minusValue = 0;
+    for(let i=0 ; i< recordValues.length ; i++){
+        if(parseInt(recordValues[i].innerHTML) >= 0){
+        incomeValue += parseInt(recordValues[i].innerHTML) ;
+        income.innerHTML = incomeValue;
+        }
+        else{
+        minusValue += parseInt(recordValues[i].innerHTML);
+        minus.innerHTML = minusValue;
+        }
+    }
+    
+}
 
 function handleSubmit(e){
     e.preventDefault();
@@ -14,23 +33,27 @@ function handleSubmit(e){
     span1.className = "record_name";
     span1.innerHTML = recordName.value;
     span2.id = "recordValue";
-    span2.className = "record.value";
+    span2.className = "record_value";
+    div.appendChild(span1);
     if(recordValue.value < 0){
         div.style.borderRight = "solid 4px #C0392B";
         span2.innerHTML = recordValue.value;
+        div.appendChild(span2);
+
     }
     else{
         span2.innerHTML = `+${recordValue.value}`;
+        div.appendChild(span2);
     } // 양수와 음수에 따른 표기 구별
-    div.appendChild(span1);
-    div.appendChild(span2);
     histories.appendChild(div);
     recordName.value = "";
     recordValue.value = null; //내역 추가
+    totalIncome()
 }
 
 function init(){
     addForm.addEventListener("submit", handleSubmit);
+
 }
 
 init();
