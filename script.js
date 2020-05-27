30cfee1af25320fd3377ab651ec53bd45ec8ee59
Choi Jiwon <winning1993@naver.com>
2020-05-27 17:58:05 +0900

    양수와 음수에 따른 표기 구별

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const addForm = document.getElementById("addForm"),
-histories = document.getElementById("histories")
+histories = document.getElementById("histories"),
+recordName = document.getElementById("recordName"),
+recordValue = document.getElementById("recordValue");
 
 function handleSubmit(e){
     e.preventDefault();
-    const recordName = e.target[0].value,
-    recordValue = e.target[1].value;
     const div = document.createElement("div"),
     span1 = document.createElement("span"),
     span2 = document.createElement("span");
@@ -12,13 +12,21 @@ function handleSubmit(e){
     div.className = "record";
     span1.id = "recordName";
     span1.className = "record_name";
-    span1.innerHTML = recordName;
+    span1.innerHTML = recordName.value;
     span2.id = "recordValue";
     span2.className = "record.value";
-    span2.innerHTML = recordValue;
+    if(recordValue.value < 0){
+        div.style.borderRight = "solid 4px #C0392B";
+        span2.innerHTML = recordValue.value;
+    }
+    else{
+        span2.innerHTML = `+${recordValue.value}`;
+    } // 양수와 음수에 따른 표기 구별
     div.appendChild(span1);
     div.appendChild(span2);
     histories.appendChild(div);
+    recordName.value = "";
+    recordValue.value = null; //내역 추가
 }
 
 function init(){
